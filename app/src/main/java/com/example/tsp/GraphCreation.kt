eb5a820d004221e4af43df2ec62b1ecc038eb930
SNIAPA <wasilewskihubers@gmail.com>
2022-12-04 19:35:48 +0100

    ui adjustment

--- app/src/main/java/com/example/tsp/GraphCreation.kt
+++ app/src/main/java/com/example/tsp/GraphCreation.kt
@@ -1,6 +1,11 @@
 package com.example.tsp
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.text.PrecomputedText.Params
+import android.util.AttributeSet
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -40,6 +45,7 @@ class GraphCreation : Fragment() {
     private fun addCity(name:String, lat:Double,long:Double){
 
         cities.add(City(name,lat,long))
+
 
         val cityRow = TableRow(context)
         cityRow.addView(TextView(context).apply { text = name;gravity=1 })
