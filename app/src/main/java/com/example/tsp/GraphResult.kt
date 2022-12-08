e1bd6adcac81d819c5967e0f14d82b7aa7a915fc
SNIAPA <wasilewskihubers@gmail.com>
2022-12-08 09:10:25 +0100

    beta done

--- app/src/main/java/com/example/tsp/GraphResult.kt
+++ app/src/main/java/com/example/tsp/GraphResult.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 
 class GraphResult : Fragment() {
 
@@ -12,17 +14,50 @@ class GraphResult : Fragment() {
         var ans : Pair<List<Int>,Double>? = null
     }
 
+    lateinit var ansTextView : TextView
+    lateinit var backButton : Button
+
+    private fun initializeComponents(view: View){
+
+        with(view){
+
+            ansTextView = findViewById(R.id.ansTextView)
+            backButton = findViewById(R.id.backButton)
+
+
+            backButton.setOnClickListener {
+
+                parentFragmentManager.beginTransaction().replace(R.id.fragmentContainer,GraphCreation()).commit()
+
+            }
+
+
+        }
+
+
+
+    }
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+
+        with(inflater.inflate(R.layout.fragment_graph_result, container, false)){
+
+            initializeComponents(this)
+
+
+            ansTextView.text = ans.toString()
+
+            return this
+        }
 
 
 
 
-        return inflater.inflate(R.layout.fragment_graph_result, container, false)
+
     }
 
 }
